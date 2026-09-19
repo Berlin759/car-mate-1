48603ed862a7ce7ea430547b1baed0a48c7e8866
ronak.dobariya <ronak.dobariya@bitrixinfotech.com>
2026-09-19 17:08:31 +0530

    TC

--- car-owner/controllers/owner.controller.js
+++ car-owner/controllers/owner.controller.js
@@ -5443,12 +5443,13 @@ export const postVerifyRazorPaySignature = async (req, res) => {
                 $project: {
                     date: 1,
                     slot: 1,
-                    totalAmount: {
-                        $subtract: [
-                            { $ifNull: ["$totalAmount", 0] },
-                            { $ifNull: ["$discountAmount", 0] },
-                        ],
-                    },
+                    totalAmount: 1,
+                    // totalAmount: {
+                    //     $subtract: [
+                    //         { $ifNull: ["$totalAmount", 0] },
+                    //         { $ifNull: ["$discountAmount", 0] },
+                    //     ],
+                    // },
                     status: 1,
                     createdAt: 1,
                     serviceDetails: {
